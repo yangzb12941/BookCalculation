e99770a69a62cbee69db78dc37145ac08f3fc37a
yangzb12941 <1294043509@qq.com>
2022-03-30 17:50:33 +0800

    2022-03-30: 1、新增web服务的Controller；

--- webControl/src/main/java/common/Constants.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-import java.io.File;
-
-public class Constants {
-    public static final String TemplateFile = "templateFile"+ File.separator;
-    public static final String Download = "download"+ File.separator;
-}
